aab46c2085b8b0a8703c5820aa0a6db03310f8a9
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-08-19 19:36:48 -0300

    OT281_15 Code Fix
    2 sql files were modified
    
    All the transformation in data were replaced to RAW data
    The fields that cannot be loaded from the main table without any join or subquery are set to NULL

--- airflow/include/C_uni_de_palermo.sql
+++ airflow/include/C_uni_de_palermo.sql
@@ -1,24 +1,21 @@
 
 select
-names,
-sexo,
-correos_electronicos,
-birth_dates,
-universidad,
-fecha_de_inscripcion,
+universidad as university,
 careers,
-palermo_tres_de_febrero.codigo_postal,
-localidad
+fecha_de_inscripcion as inscription_date,
+names as last_name,
+sexo as gender,
+null as age,
+birth_dates,
+codigo_postal as postal_code,
+null as location,
+correos_electronicos as email
+
 from palermo_tres_de_febrero
-inner join
-localidad2
-on localidad2.codigo_postal = cast(palermo_tres_de_febrero.codigo_postal as int)
 where universidad='_universidad_de_palermo' and to_date(fecha_de_inscripcion ,'DD/Mon/YY') between '2020-09-01' and '2021-02-01';
 
 
 
--- The query load information from palermo_tres_de_febrero table and the localidad2 table using the codigo_postal column
--- The ON condition to join the tables has to be affected by the CAST function because location fields in palermo_tres_de_febrero were in lower case and localidad in localidad2 were in upper case
-
+-- The query load information from palermo_tres_de_febrero table 
 -- The where condition uses the inscription_date column but it has to been transformed in date format first
 
--- airflow/include/C_uni_nacional_de_jujuy.sql
+++ airflow/include/C_uni_nacional_de_jujuy.sql
@@ -2,20 +2,18 @@ SELECT
 university,
 career,
 inscription_date ,
-nombre,
-sexo,
+nombre as last_name,
+sexo as gender,
 birth_date ,
-codigo_postal,
+NULL as age,
+NULL as postal_code,
 "location",
 email    
 from public.jujuy_utn
-inner join localidad2
-on localidad2.localidad = upper(jujuy_utn."location")
 where university = 'universidad nacional de jujuy' and to_date(inscription_date,'YYYY/MM/DD') between '2020-09-01' and '2021-02-01' ;
 
 
--- The query load information from jujuy_utn table and join it with localidad2 table using the location field
--- The ON condition to join the tables has to be affected by the UPPER function because location fields in jujuy_utn were in lower case and localidad in localidad2 were in upper case
+-- The query load information from jujuy_utn table 
 -- The where condition uses the inscription_date column but it has to been transformed in date format first
 
 
