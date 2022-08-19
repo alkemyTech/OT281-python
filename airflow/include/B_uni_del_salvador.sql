4cc9dcd6277d3faa5c7717fac833bb58deac0376
gioleon <giovannileon2017@gmail.com>
2022-08-19 14:15:30 -0500

    fixes addition of fixes

--- airflow/include/B_uni_del_salvador.sql
+++ airflow/include/B_uni_del_salvador.sql
@@ -2,16 +2,14 @@ SELECT
        svm.universidad AS university, 
        svm.carrera AS career,
        svm.fecha_de_inscripcion AS inscription_date,
-       svm.nombre as first_name,
        svm.nombre AS last_name,
        svm.sexo AS gender,
-       svm.fecha_nacimiento as age,
-       l2.codigo_postal AS postal_code,
+       svm.fecha_nacimiento as birth_day,
+       NULL AS age,
+       NULL AS postal_code,
        svm.localidad AS location,
        svm.email AS email
 FROM salvador_villa_maria AS svm
-LEFT JOIN localidad2 AS l2
-ON replace(svm.localidad, '_', ' ') = l2.localidad
 WHERE TO_DATE(svm.fecha_de_inscripcion, 'DD-Mon-YY') >= '2020/09/01' 
 AND TO_DATE(svm.fecha_de_inscripcion, 'DD-Mon-YY') <= '2021/02/01'
 AND svm.universidad like 'UNIVERSIDAD_DEL_SALVADOR';
--- airflow/include/B_uni_nacional_del_comahue.sql
+++ airflow/include/B_uni_nacional_del_comahue.sql
@@ -2,16 +2,14 @@ SELECT
        fc.universidad AS university, 
        fc.carrera AS career,
        fc.fecha_de_inscripcion AS inscription_date,
-       fc.name AS first_name,
        fc.name AS last_name,
        fc.sexo AS gender,
-       fc.fecha_nacimiento AS age,
+       fc.fecha_nacimiento AS birth_day,
+       NULL AS age,
        fc.codigo_postal AS postal_code,
-       l2.localidad AS location,
+       NULL AS location,
        fc.correo_electronico AS email
 FROM flores_comahue AS fc
-LEFT JOIN localidad2 AS l2
-ON CAST(fc.codigo_postal AS INTEGER) = l2.codigo_postal
 WHERE fecha_de_inscripcion >= '2020/09/01' 
 AND fecha_de_inscripcion <= '2021/02/01'
 AND universidad like 'UNIV. NACIONAL DEL COMAHUE';
