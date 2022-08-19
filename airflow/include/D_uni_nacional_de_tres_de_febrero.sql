a3a5291b54f0e952b58e598aa77a2892a2513de7
Juan Ignacio Hernández <hernandezjuanvt@gmail.com>
2022-08-19 16:23:10 -0300

    Fixed SQL script for Universidad Nacional Tres de Febrero

--- airflow/include/D_uni_nacional_de_tres_de_febrero.sql
+++ airflow/include/D_uni_nacional_de_tres_de_febrero.sql
@@ -2,21 +2,22 @@
 -- 'Universidad Nacional de Tres de Febrero' inscripted between September 1, 2020 and February 1, 2021.
 
 SELECT universidad AS university,
-careers AS career,
-fecha_de_inscripcion AS inscription_date,
-names AS last_name, 
-sexo AS gender,
-birth_dates AS age,
-l.codigo_postal AS postal_code, 
-l.localidad AS location,
-correos_electronicos AS email
-FROM palermo_tres_de_febrero as ptdf 
-JOIN localidad2 AS l
-ON CAST (ptdf.codigo_postal AS INT) = l.codigo_postal
-WHERE universidad = 'universidad_nacional_de_tres_de_febrero'
-AND CAST (fecha_de_inscripcion AS DATE) >= '2020/09/01'
-AND CAST (fecha_de_inscripcion AS DATE) <= '2021/02/01';
+    careers AS career,
+    fecha_de_inscripcion AS inscription_date,
+    NULL AS first_name,
+    names AS last_name, 
+    sexo AS gender,
+    birth_dates AS birth_date,
+    NULL AS age,
+    codigo_postal AS postal_code,
+    NULL AS location,
+    correos_electronicos AS email
+FROM palermo_tres_de_febrero
+WHERE LOWER(universidad) = 'universidad_nacional_de_tres_de_febrero'
+    AND CAST (fecha_de_inscripcion AS DATE) >= '2020/09/01'
+    AND CAST (fecha_de_inscripcion AS DATE) <= '2021/02/01';
 
--- Note that the column 'age' is in fact the birth date, this is intentionally, since we want raw data
--- that will be processed later on further steps.
--- Same thing with 'last_name' actually storing the full name.
+-- Note that we use "LOWER(university)" to avoid caps issues while comparing strings.
+-- In the same way we cast "inscription_date" as DATE (only in the where clause),
+-- in order to avoid errors comparing dates with different formats.
+-- Not found values are stored as NULL.
