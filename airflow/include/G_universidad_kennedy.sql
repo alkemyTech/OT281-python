eef01cc6a297997216e4f23b95a919b5806a9ddd
Emanuel Schemberger <emanueldev24@gmail.com>
2022-08-19 16:43:17 -0300

    fixed identation

--- airflow/include/G_universidad_kennedy.sql
+++ airflow/include/G_universidad_kennedy.sql
@@ -1,13 +1,13 @@
 SELECT 
-	universidades       AS university,
-	trabajo             AS career,
-	nombres             AS last_name,
-    sexo                AS gender,
-    fechas_nacimiento   AS birthdate,
-    null				AS age,
-    codigos_postales    AS postal_code,
-    null      			AS location,
-    emails              AS email,
+    universidades      AS university,
+    trabajo            AS career,
+    nombres            AS last_name,
+    sexo               AS gender,
+    fechas_nacimiento  AS birthdate,
+    null			   AS age,
+    codigos_postales   AS postal_code,
+    null      		   AS location,
+    emails             AS email,
     to_date(knd.fechas_de_inscripcion, 'YY-MON-DD') as inscription_date
 FROM uba_kenedy as knd
 WHERE knd.universidades = 'universidad-j.-f.-kennedy' -- filter by university
