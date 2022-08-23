874a5c7957df2cabc49ed448c2d0ffe46734fb42
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-08-23 13:48:42 -0300

    set conection with hook and python operator

--- airflow/dags/C_uni_nacional_de_jujuy.py
+++ airflow/dags/C_uni_nacional_de_jujuy.py
@@ -1,20 +1,56 @@
-# Import timedelta to establish the schedule
-from datetime import timedelta
-#Import airflow modules 
+
+from datetime import datetime
+from msilib import schema
 from airflow import DAG
-from airflow.utils.dates import days_ago
-# This module is used for dummy Operatos
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-#Define DAG 
+#import logging module
+import logging
+
+
+
+#Function to open the sql file and save it in a variable
+def sql_reader(file):
+    sql_file = open(file)
+    return sql_file.read()
+
+
+#Function to fetch data from Postgress 
+def get_postgress_data():
+    file = "../include/C_uni_nacional_de_jujuy.sql"
+    sql_query = sql_reader(file)
+    logging.debug(f'The query file is {sql_query}')
+    # SET CONN ID WITH THE AIRFLOW CONN ID 
+    pg_hook = PostgresHook(
+        postgres_conn_id='S3_postgres_OT281',
+        schema='training'
+    )
+    pg_conn = pg_hook.get_conn()
+    logging.debug(f'The hook params are: {postgres_conn_id} and schema {schema}')
+    cursor = pg_conn.cursor()
+    logging.debug("cursor was created successfully")
+    cursor.execute(sql_query)
+    return cursor.fetchall()
+
+default_args={
+    'owner':'airflow',
+    'retries':5,
+    'retry_delay':5
+}
+
+
 with DAG(
-    dag_id='C_uni_nacional_de_jujuy',
-    description='Dag with no operator related to Universidad Nacional de Jujuy',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(2),
-    tags=['C_uni_nacional_de_jujuy'],
-
-)  as dag_C_uni_de_palermo:
-# Set dummy operator    
-    empty_dag_C_uni_de_palermo= DummyOperator(task_id='C_uni_nacional_de_jujuy')
-
+    dag_id='C_uni_nacional_de_jujuy_',
+    description='DAG to load data from Universiodad Nacional de Jujuy from postgress',
+    schedule_interval='@daily',
+    start_date=datetime(year=2022, month=8, day=22),
+    default_args=default_args,
+    catchup=False
+) as dag:
+    task_C_uni_nacional_de_jujuy_load_query = PythonOperator(
+	task_id='C_uni_nacional_de_jujuy_load_query',
+	python_callable=get_postgress_data,
+	do_xcom_push=True
+)
