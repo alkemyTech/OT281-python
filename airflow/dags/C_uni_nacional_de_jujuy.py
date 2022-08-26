a85a1c59bc26fbf0063c4506a494fd3254503b47
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-08-25 22:55:52 -0300

    The folders and files are set to be relative

--- airflow/dags/C_uni_nacional_de_jujuy.py
+++ airflow/dags/C_uni_nacional_de_jujuy.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from datetime import datetime
 from airflow import DAG
@@ -27,8 +28,10 @@ def sql_reader(file):
 
 #Function to fetch data from Postgress 
 def get_postgress_data():
-    #file contains the path to the sql query for this university , could be changed to a dinamic option
-    file = "/home/ariel/alkemy/entorno_python3_9/OT281-python/airflow/include/C_uni_nacional_de_jujuy.sql"
+    #file contains the path to the sql query for this university 
+    #file = "/home/ariel/alkemy/entorno_python3_9/OT281-python/airflow/include/C_uni_nacional_de_jujuy.sql"
+    file_name_sql="C_uni_nacional_de_jujuy.sql"
+    file = os.path.join(os.path.dirname(__file__), '../include/'+file_name_sql)
     sql_query = sql_reader(file)
     logging.debug(f'The query is {sql_query}')
     # CREATE PostgressHook instance
@@ -40,8 +43,9 @@ def get_postgress_data():
     #CONNECT TO THE DDBB
     pg_conn = pg_hook.get_conn()
     # SET FILE NAME FOR CSV WITH RAW DATA
-    # PATH AND NAME SHOULD BE SET BY ARGUMENT
-    filename=r"/home/ariel/alkemy/entorno_python3_9/OT281-python/airflow/files/C_uni_nacional_de_jujuy.csv"
+    file_name_csv="C_uni_nacional_de_jujuy.csv"
+    filename = os.path.join(os.path.dirname(__file__), '../files/'+file_name_csv)
+    #filename=r"/home/ariel/alkemy/entorno_python3_9/OT281-python/airflow/files/C_uni_nacional_de_jujuy.csv"
     # FORMAT THE QUERY TO MAKE IT WORK WITH copy_expert
     new_sql_query= "COPY ( "+sql_query+" ) TO STDOUT WITH CSV HEADER"    
     # REMOVE " ; " CHAR TO GET IT WORK
