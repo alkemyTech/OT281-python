0280947c230677a929708467fb0ca7025407cf09
Giovanni Leon <giovannileon2017@gmail.com>
2022-09-01 14:07:47 -0500

    0T281-64

--- airflow/dags/B_uni_nacional_del_comahue.py
+++ airflow/dags/B_uni_nacional_del_comahue.py
@@ -6,12 +6,14 @@ DAG workflow:
 - Load data into s3 bucket
 """
 
+import pandas as pd
 import logging
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from pathlib import Path
+import os
 
 # Set up logger
 logger = logging.getLogger(__name__)
@@ -61,8 +63,116 @@ def sql_queries():
 
 # Pandas data wrangling
 def data_transformation():
+    
+    # Parent folder
+    parent_path = Path(__file__).parent.parent
+    
+    # CSV files
+    path_university = Path(
+        parent_path,
+        'files/B_uni_nacional_del_comahue.csv'
+    )
 
-    pass
+    path_location = Path(
+        parent_path,
+        'assets/codigos_postales.csv'
+    )
+
+    path_to_save = Path(
+        parent_path,
+        'datasets/B_uni_nacional_del_comahue.txt'
+    )
+
+    # Postal code
+    locations = pd.read_csv(path_location)    # Read csv
+
+    locations['localidad'] = locations['localidad'].apply(str.lower)
+
+    # University
+    df = pd.read_csv(path_university, encoding = 'UTF8')
+    
+    # Columns with string data to transform
+    str_columns = [
+        'university', 'career',
+        'last_name', 'location',
+        'email'
+    ]
+
+    df[str_columns] = df[str_columns].astype('string')    # Object -> str
+
+    df[str_columns] = df[str_columns].apply( 
+        lambda x: x.str.lower()
+        .str.replace('_', ' ')
+        .str.replace('-', ' ')
+        .str.strip()
+    )    # Delete dashes, underscores and upper -> lower
+
+    # First name
+    df['first_name'] = df['last_name'].apply(
+        lambda x: x.split()[0]
+    ).astype('string')
+
+    # Last name
+    df['last_name'] = df['last_name'].apply(
+        lambda x: x.split()[1]
+    ).astype('string')
+
+    # Gender
+    df['gender'] = df['gender'].str.replace('M', 'male').replace(
+        'F', 'female'
+    ).astype('string')
+
+    # Inscription date
+    df['inscription_date'] = df['inscription_date'].astype('string')
+    
+    # Age
+    """
+    To generate the age, first 
+    we have to transform birth_date column
+    """
+    
+    df['birth_date'] = pd.to_datetime(
+        df['birth_date'], 
+        format = '%Y-%m-%d'
+    ).dt.date
+
+    df['age'] = (datetime.now().date() - df['birth_date']).dt.days//365
+
+    # Obtain location from locations df
+    merged_df = df.merge(
+        locations, how = 'left', 
+        left_on='postal_code', 
+        right_on='codigo_postal'
+    )
+
+    # Assing localidad column to location
+    merged_df['location'] = merged_df['localidad'].astype('string')
+
+    # Change postal_code data type
+    merged_df['postal_code'] = merged_df['postal_code'].astype('string')
+
+    # Drop codigo_postal and localidad columns
+    merged_df = merged_df.drop(
+        ['codigo_postal', 'localidad']
+        , axis = 1
+    )
+    
+    # Make sure that the folder where data will be saved exists
+    if not os.path.exists(os.path.join(parent_path, 'datasets')):
+        os.mkdir(os.path.join(parent_path, 'datasets'))
+    
+    # Save data
+    cols_of_insterest = [
+        'university', 'career',
+        'inscription_date', 'first_name',
+        'last_name', 'gender',
+        'age', 'postal_code',
+        'location', 'email'
+    ]
+
+    merged_df[cols_of_insterest].to_csv(
+        path_to_save, encoding='UTF8'
+    )
 
 
 # Load data into s3
