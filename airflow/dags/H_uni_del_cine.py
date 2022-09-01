329c7c19487258360a021d146c7d523f262d6236
agusorsato <agustinorsato@gmail.com>
2022-09-01 12:30:49 -0300

    OT281-60
    COMO: Analista de datos
    QUIERO: Implementar el Python Operator
    PARA: procesar los datos obtenidos de la base de datos dentro del DAG
    
    Criterios de aceptación:
    Configurar el Python Operator para que ejecute las dos funciones que procese los datos para las siguientes universidades:
    
    Universidad Del Cine
    
    Universidad De Buenos Aires
    
    Documentación
    
    https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/operators/python/index.html#module-airflow.operators.python

--- airflow/dags/H_uni_del_cine.py
+++ airflow/dags/H_uni_del_cine.py
@@ -4,19 +4,19 @@
 2- Configure retry for DAG tasks
 3- Configure the registry
 """
-#Time functions
+# Time functions
 from datetime import timedelta, datetime
-#Airflow
+# Airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-#manager Folders
+# manager Folders
 import os
-#Pandas from datafrrame
+# Pandas from datafrrame
 import pandas as pd
-#Logging
+# Logging
 import logging
-
+from dateutil.relativedelta import relativedelta
 
 
 # Config logging
@@ -25,90 +25,101 @@ logger.setLevel(logging.DEBUG)
 
 sh = logging.StreamHandler()
 sh.setLevel(logging.DEBUG)
-#Format
-formatter = logging.Formatter('%(asctime)s-%(levelname)s-%(message)s', datefmt='%Y/%m/%d')
+# Format
+formatter = logging.Formatter(
+    '%(asctime)s-%(levelname)s-%(message)s', datefmt='%Y/%m/%d')
 # add format a sh
 sh.setFormatter(formatter)
 
-#this function open csvfile and transform
+# Path
+current_dir = os.path.dirname(os.path.normpath(__file__))
+ROOT_FOLDER = os.path.abspath(os.path.join(current_dir, os.pardir))
+FILE_NAME = 'H_uni_del_cine'
+
+
+# this function open csvfile and transform
 def open_csv():
     file_name = "H_uni_del_cine.csv"
-    filename = os.path.join(os.path.dirname(__file__),'../files/'+file_name)
+    filename = os.path.join(ROOT_FOLDER, '../files/'+file_name)
     H_uni_del_cine = pd.read_csv(filename)
     return True
-#this function open the sqlFile
+# this function open the sqlFile
+
+
 def sql_reader(file):
     sql_file = open(file)
     return sql_file.read()
 
 
-#Def extract
-def extract_data ():
-    #File .sql query
-   file_sql = "H_uni_del_cine.sql"
-    #dir .sql query
-   file = os.path.join(os.path.dirname(__file__),'../include/'+file_sql)
-    #read .sql query
-   sql_query = sql_reader(file)
+# Def extract
+def extract_data():
+    # File .sql query
+    file_sql = "H_uni_del_cine.sql"
+    # read .sql query
+    sql_query = sql_reader(f'{ROOT_FOLDER}/include/{file_sql}')
 
-   logging.debug(f"The query use {sql_query}")
+    logging.debug(f"The query use {sql_query}")
 
-    #PostgresHook instance
-   pg_hook = PostgresHook(
-            postgres_conn_id = 'db_universidades_postgres',
-            )
-    #conect to the db
-   pg_conn = pg_hook.get_conn()
-    #csv with raw data
-   file_name="H_uni_del_cine.csv"
-    
-   filename = os.path.join(os.path.dirname(__file__), '../files/'+file_name)
+    # PostgresHook instance
+    pg_hook = PostgresHook(
+        postgres_conn_id='db_universidades_postgres',
+    )
+    # conect to the db
+    pg_conn = pg_hook.get_conn()
+    # csv with raw data
+    file_name = "H_uni_del_cine.csv"
 
-   new_sql_query= "COPY ( "+sql_query+" ) TO STDOUT WITH CSV HEADER"  
-    # remove " ; " 
-   new_sql_query=new_sql_query.replace(";","")
+    filename = f'{ROOT_FOLDER}/files/{file_name}' #
+
+    new_sql_query = "COPY ( "+sql_query+" ) TO STDOUT WITH CSV HEADER"
+    # remove " ; "
+    new_sql_query = new_sql_query.replace(";", "")
     # execute copy_expert
-   pg_hook.copy_expert(new_sql_query,filename)
+    pg_hook.copy_expert(new_sql_query, filename)
 
-   return True
+    return True
+####################################################################################
+
+
 
 
 # dag Config
-default_args={
-    #"owner": "airflow-OT281-28",
-    "retries": 5, # This comes from the SQL definition
-    "retry_delay": timedelta(seconds=5) #fer 5 seg
+default_args = {
+    # "owner": "airflow-OT281-28",
+    "retries": 5,  # This comes from the SQL definition
+    "retry_delay": timedelta(seconds=5)  # fer 5 seg
 }
 
-#Init DAG
-with DAG( 
-          dag_id='H_universidad_del_cine', 
-          default_args=default_args,
-          description='Consulta a la Universidad del Cine',
-          schedule_interval='@hourly', ## DAG should be run every 1 hour, every day
-          start_date=datetime(year=2022,month=8,day=19), ## YOU HAVE TO DEFINE THE DATE
-          catchup=False
-         ) as dag:
+# Init DAG
+with DAG(
+    dag_id='H_universidad_del_cine',
+    default_args=default_args,
+    description='Consulta a la Universidad del Cine',
+    schedule_interval='@hourly',  # DAG should be run every 1 hour, every day
+    # YOU HAVE TO DEFINE THE DATE
+    start_date=datetime(year=2022, month=8, day=19),
+    catchup=False
+) as dag:
 
-        #0 - init Log
-        logger.debug("H_universidad_del_cine start")
+    # 0 - init Log
+    logger.debug("H_universidad_del_cine start")
 
-        #1 - Data from postgres database
-        udc_extract_data=PythonOperator(
-            task_id = 'extract_data_H_universidad_del_cine',
-            python_callable = extract_data
-        )
-           
-        #2 - Transforms Data
-        udc_pandas_transform=PythonOperator(
-            task_id = 'transform_data_H_universidad_del_cine',
-            python_callable = open_csv
-        )
+    # 1 - Data from postgres database
+    udc_extract_data = PythonOperator(
+        task_id='extract_data_H_universidad_del_cine',
+        python_callable=extract_data
+    )
 
-        #3 - Load Data
-        #udc_load_data=PythonOperator(
-        #)
+    # 2 - Transforms Data
+    #udc_pandas_transform = PythonOperator(
+       # task_id='transform_data_H_universidad_del_cine',
+      #  python_callable=transform_data_pd
+    #)
 
-        #4 - The execution order of the DAG
-        udc_extract_data >> udc_pandas_transform 
-        #>> udc_load_data
+    # 3 - Load Data
+    # udc_load_data=PythonOperator(
+    # )
+
+    # 4 - The execution order of the DAG
+    udc_extract_data #>> udc_pandas_transform
+    # >> udc_load_data
