a8b7f31463e4c7b16df1b426cf05cade1729a5d4
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-09-02 07:27:49 -0300

    create generators file

---
+++ airflow/plugins/dynamic_dags/C_generators.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment,FileSystemLoader
+import yaml
+import os
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+env = Environment(loader=FileSystemLoader(file_dir))
+template = env.get_template('C_template.jinja2')
+
+for filename in os.listdir(file_dir):
+        if filename.endswith(".yaml"):
+            with open(f"{file_dir}/{filename}","r")as configfile:
+                config = yaml.safe_load(configfile)
+                with open(f"{file_dir}/../../dags/{config['dag_id']}.py","w") as f:
+                    f.write(template.render(config))
