a70c3c7311063037d08deaabfd16c4bf63d7bd77
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-09-03 12:19:31 -0300

    generators file set only for C group university

--- airflow/plugins/dynamic_dags/C_generators.py
+++ airflow/plugins/dynamic_dags/C_generators.py
@@ -7,7 +7,7 @@ env = Environment(loader=FileSystemLoader(file_dir))
 template = env.get_template('C_template.jinja2')
 
 for filename in os.listdir(file_dir):
-        if filename.endswith(".yaml"):
+        if filename.endswith(".yaml") and filename.startswith("C_"):
             with open(f"{file_dir}/{filename}","r")as configfile:
                 config = yaml.safe_load(configfile)
                 with open(f"{file_dir}/../../dags/{config['dag_id']}.py","w") as f:
