649901ceb3c7c2abe5a72ee2d647bb5989118cb2
agusdiazb <agustindiazbarquinero@gmail.com>
2022-09-06 19:33:25 -0300

    OT281-50&58

--- airflow/dags/F_uni_de_moron.py
+++ airflow/dags/F_uni_de_moron.py
@@ -1,17 +1,28 @@
 '''
 COMO: Analista de datos
-QUIERO: Configurar los log
-PARA: Mostrarlos en consola
-QUIERO: Configurar los retries con la conexión al a base de datos
-PARA: poder intentar nuevamente si la base de datos me produce un error
+QUIERO: Implementar SQL Operator
+PARA: tomar los datos de las bases de datos en el DAG
+
 '''
 
-#Imports
+#Airflow Imports
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+# Other imports
 from datetime import datetime, timedelta
 import logging
+import os
+from pathlib import Path
 
+
+
+#Connection id for Postgres Database
+POSTGRES_CONN_ID = "db_universidades_postgres"
+
+# Filename
+file_name = 'F_uni_de_moron'
 
 logging.basicConfig(level=logging.INFO, datefmt= '%Y-%m-%d',
                     format='%(asctime)s - %(name)s - %(message)s') 
@@ -28,12 +39,30 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 5,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(seconds=5)
 }
 
 # SQL extraction
 def sql_query():
-    pass
+    pg_hook= PostgresHook.get_hook(POSTGRES_CONN_ID)
+   
+    #Log
+    logging.info('Exporting query to file')
+
+    #Sql path
+    sql_path = os.path.join(os.path.dirname(os.path.normpath(__file__)).rstrip('/dags'), 'include/'+ file_name+ '.sql')
+    
+
+    #csv path
+    csv_path = os.path.join(os.path.dirname(os.path.normpath(__file__)).rstrip('/dags'), 'files/' + file_name + '.csv')
+
+    #Open and read sql file
+    with open(sql_path, "r") as file:
+        sql = file.read()
+        sql = "COPY (" + sql.replace(";", "") + ") TO STDOUT WITH CSV HEADER" 
+    
+    #Run query and storage it in csv file
+    pg_hook.copy_expert(sql, csv_path)
 
 #Pandas processing and Transformation
 def pandas_process():
@@ -46,7 +75,7 @@ def load_to_S3():
 
 #Instantiate DAG
 with DAG('F_uni_de_moron',
-         description = ' dag for Universidad de Morón' 
+         description = ' dag for Universidad de Moron',
          start_date=datetime(2022, 8, 23),
          schedule_interval='@hourly',
          default_args = default_args,
@@ -54,13 +83,14 @@ with DAG('F_uni_de_moron',
         ) as dag:
 
 
+        
         #First task: Extraction. (PostgresOperator could be used)
-        opr_sql_query = PythonOperator(
+        opr_sql_query = PythonOperator(    
             task_id='sql_query',
             python_callable = sql_query
         )
         
-        #Second Task: Pandas Processing and Transformation
+        #Second Task: Pandas Processing and Transformation.
         opr_pandas_process = PythonOperator(
             task_id='pandas_process',
             python_callable = pandas_process
