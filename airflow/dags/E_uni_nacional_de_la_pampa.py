e1591319a1aade99b0f0899762aa106ddff84057
Lino Cruz <65975099+LinoCruz@users.noreply.github.com>
2022-09-06 14:40:58 -0500

    OT281-49 Sql E (#75)

--- airflow/dags/E_uni_nacional_de_la_pampa.py
+++ airflow/dags/E_uni_nacional_de_la_pampa.py
@@ -8,10 +8,12 @@
 
 from datetime import timedelta, datetime
 import logging
+import os
 
 from airflow import DAG
 
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 #from airflow.hooks.S3_hook import S3Hook
 
 #Logs config
@@ -29,7 +31,28 @@ default_args = {
 
 #Get data and read it with SQL query
 def sql_query():
-    pass
+
+    #Get the Sql query from "include" folder
+    sql_query = os.path.join(os.path.dirname(os.path.normpath(__file__)).rstrip('/dags'), 'include/'+"E_uni_nacional_de_la_pampa.sql")
+    sql_file = open(sql_query)
+    sql_file = sql_file.read()
+    sql_file = "COPY (" + sql_file.replace(";", "") + ") TO STDOUT WITH CSV HEADER"
+
+
+    #Hook Creation and Connection with Postgress
+    hook = PostgresHook(
+        postgres_conn_id='db_universidades_postgres',
+        schema='training'
+    )
+    logging.info("SQL succesfully extract")    
+
+
+    #Export raw data in CSV format to the "files" directory
+    filename_csv="E_uni_nacional_de_la_pampa.csv"
+    filename = os.path.join(os.path.dirname(os.path.normpath(__file__)).rstrip('/dags'), 'files/'+filename_csv)
+
+    # Return the data from the hook using the sql querys and export it in csv
+    return hook.copy_expert(sql_file, filename)
 
 #Transform data with pandas
 def transform_data():
@@ -67,6 +90,7 @@ with DAG(
 
 '''
 Resources:
-a. DAG to Upload to S3 [https://betterdatascience.com/apache-airflow-amazon-s3/] 
+a. DAG to Upload to S3 [https://betterdatascience.com/apache-airflow-amazon-s3/]
+b. Logs configuration[https://docs.python.org/3/howto/logging.html]
+c. Connection to Postgress[https://registry.astronomer.io/providers/postgres/modules/postgreshook]
 '''
-
