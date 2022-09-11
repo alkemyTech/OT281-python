56a0557c2f63d90be025c833bdb7301ca2008132
manoloacademia <luispablosegovia@icloud.com>
2022-09-11 18:37:30 -0300

    Finished top10 for tags and answers count

--- bigdata/src/A_top10_tags_accepted_answers.py
+++ bigdata/src/A_top10_tags_accepted_answers.py
@@ -1,10 +1,10 @@
-""" This file shows the top 10 tags with maximum accepted answers.
-"""
+""" This file shows the top 10 tags with maximum accepted answers."""
+
 # Import libraries
-from datetime import datetime
+from operator import itemgetter
 from pathlib import Path
-from typing import Counter
 import os
+import pandas as pd
 import re
 import xml.etree.ElementTree as ET
 from functools import reduce
@@ -18,6 +18,7 @@ def chunckify(iterable,len_of_chunk):
 BASE_DIR = Path(os.path.abspath(__file__)).parent.parent
 XML_FOLDER = os.path.join(BASE_DIR, 'datasets')
 XML_FILE = os.path.join(XML_FOLDER,'posts.xml')
+CSV_FILE = os.path.join(BASE_DIR, 'output', 'A_top10_tags_accepted_answers.csv')
 
 # Root row keys
 """
@@ -27,8 +28,11 @@ XML_FILE = os.path.join(XML_FOLDER,'posts.xml')
 """
 
 # Helper functions to map and reduce
-
 def get_tags_and_answers(data):
+    """
+    This funtion takes the data from the root xml post file
+    and returns the answercount per tag.
+    """
     try:
         tags = data.attrib['Tags']
     except:
@@ -41,17 +45,23 @@ def get_tags_and_answers(data):
     return tags, accepted_answers
 
 def separate_tags_and_answers(data):
+    """ This function returns a dictionary with tags as key and answer count as value."""
     return dict([[tag, data[1]] for tag in data[0]])
 
 def reduce_tags_and_answers(data1, data2):
-    for key in data2.keys():
+    """
+    This function compares two dictionaries. If the keys are equal, it sums the values.
+    Else, it updates the key-value pair.
+    """
+    for key, value in data2.items():
         if key in data1.keys():
-            data1[key].update({key: data1[key] + data2[key]})
+            data1[key] += value
         else:
-            data1.update(data2.items())
+            data1.update({key: value})
     return data1
 
 def mapper(data):
+    """ This function maps the tag and answer count and reduces it into one dictionary."""
     mapped_tags_and_answers = list(map(get_tags_and_answers, data))
     mapped_tags_and_answers = list(filter(None, mapped_tags_and_answers))
     answers_counted_per_tag = list(map(separate_tags_and_answers, mapped_tags_and_answers))
@@ -61,15 +71,22 @@ def mapper(data):
         return
     return reduced
 
-def calculate_top10(data):
-    return data[0], data[1]
-
 if __name__ == '__main__':
     
     # Get root from xml file and chunkify it
     tree = ET.parse(XML_FILE)
     root = tree.getroot()
     data_chunks = chunckify(root, 50)
+
+    # Apply MapReduce functionality
     mapped = list(map(mapper, data_chunks))
     mapped = list(filter(None, mapped))
-    print(mapped)
+    reduced = reduce(reduce_tags_and_answers, mapped)
+    
+    # Sort the values descending order and make the top 10
+    sorted_values = sorted(reduced.items(), key=itemgetter(1), reverse=True)
+    top10 = sorted_values[:10]
+    
+    # Create .csv file and push it into output folder
+    df = pd.DataFrame(top10, columns=['Tag', 'AnswerCount'])
+    df.to_csv(CSV_FILE)
