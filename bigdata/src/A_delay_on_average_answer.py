96ea4685f72f738a6ca361bbb084644a29e1d7a3
manoloacademia <luispablosegovia@icloud.com>
2022-09-11 11:18:13 -0300

    Added delay on average answer calculation

--- bigdata/src/A_delay_on_average_answer.py
+++ bigdata/src/A_delay_on_average_answer.py
@@ -1,14 +1,10 @@
-""" This file answers the word count in a post vs it score.
+""" This file answers the delay on average answer for each post.
 """
 # Import libraries
-from datetime import datetime
 from pathlib import Path
 from statistics import mean
-from typing import Counter
-from operator import itemgetter
-from itertools import groupby
+import dateutil.parser
 import os
-import re
 import xml.etree.ElementTree as ET
 from functools import reduce
 
@@ -31,30 +27,31 @@ XML_FILE = os.path.join(XML_FOLDER,'posts.xml')
 
 # Helper functions to map and reduce
 
-def get_score_and_words(data):
+def get_delay_time(data):
     try:
-        score = data.attrib['Score']
+        accepted_answer = data.attrib['AcceptedAnswerId']
     except:
         return
-    body = data.attrib['Body']
-    body = re.findall('(?<!\S)[A-Za-z]+(?!\S)|(?<!\S)[A-Za-z]+(?=:(?!\S))', body)
-    words = len(Counter(body))
-    return {score: [words]}
-
-def score_words_average(data1, data2):
-    for key, value in data2.items():
-        if key in data1.keys():
-            data1[key].append(value) # it adds actually a list not an int
-        else:
-            data1.update({key: value})
-    return data1
+    creation_date = data.attrib['CreationDate']
+    creation_date = dateutil.parser.parse(creation_date)
+    last_activity_date = data.attrib['LastActivityDate']
+    last_activity_date = dateutil.parser.parse(last_activity_date)
+    delay = last_activity_date - creation_date
+    return delay.days
 
 def mapper(data):
-    scored = list(map(get_score_and_words, data))
-    scored = list(filter(None, scored))
-    # scored = list(map(lambda tuple: list(tuple), scored))
-    score_avg = reduce(score_words_average, scored)
-    return score_avg
+    delayed = list(map(get_delay_time, data))
+    delayed = list(filter(None, delayed))
+    try:
+        average = mean(delayed)
+    except:
+        average = 0
+    return average
+
+def delayed_average(data1, data2):
+    data1 = (data1 + data2)/2
+    return data1
+
 
 if __name__ == '__main__':
     
@@ -63,5 +60,5 @@ if __name__ == '__main__':
     root = tree.getroot()
     data_chunks = chunckify(root, 50)
     mapped = list(map(mapper, data_chunks))
-    mapped = list(filter(None, mapped))
-    print(mapped)
+    reduced = reduce(delayed_average, mapped)
+    print(f'The average delay on answer each post is: {reduced:.2} days')
