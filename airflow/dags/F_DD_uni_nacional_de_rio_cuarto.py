d30f3b9c5a8903ffc582c39325c0679af0c514a0
Agustín <agustindiazbarquinero@gmail.com>
2022-09-14 12:09:26 -0300

    OT281-90 (#94)
    * small fixes
    
    * Dynamic DAGs configuration files
    
    * Dynamic DAGs
    
    * dataset update
    
    * sql fix & corresponding updates

--- airflow/dags/F_DD_uni_nacional_de_rio_cuarto.py
+++ airflow/dags/F_DD_uni_nacional_de_rio_cuarto.py
@@ -1,7 +1,7 @@
 '''
 COMO: Analista de datos
-QUIERO: Utilizar un operador creado por la comunidad
-PARA: poder subir el txt creado por el operador de Python al S3
+QUIERO: Arreglar un Dag dinamico
+PARA: Poder ejecutarlos normalmente
 '''
 
 #Airflow Imports
@@ -9,7 +9,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
 
 # Other imports
 from datetime import datetime, timedelta, date
@@ -29,19 +28,19 @@ s3_id = 'universidades_S3'
 s3_bucket = 'cohorte-agosto-38d749a7'
 
 # Filename
-file_name = 'F_uni_de_moron'
+file_name = "F_uni_nacional_de_rio_cuarto"
 
 #Define airflow root folder
 
 air_root_folder = os.path.dirname(os.path.normpath(__file__)).rstrip('/dags')
 
-#Log Config
+
+# Logging Set Up
 logging.basicConfig(level=logging.INFO, datefmt= '%Y-%m-%d',
-                    format='%(asctime)s - %(name)s - %(message)s') 
-             
+                    format='%(asctime)s - %(name)s - %(message)s')
 
-logger = logging.getLogger('F_uni_de_moron')
 
+logger = logging.getLogger(__name__)
 
 
 #Default settings applied to all tasks
@@ -62,7 +61,7 @@ def sql_query():
     pg_hook= PostgresHook.get_hook(POSTGRES_CONN_ID)
    
     #Log
-    logging.info('Exporting query to file')
+    logger.info('Exporting query to file')
 
     #Sql path
     sql_path = os.path.join(air_root_folder, 'include/'+ file_name+ '.sql')
@@ -70,7 +69,6 @@ def sql_query():
 
     #csv path
     csv_path = os.path.join(air_root_folder, 'files/' + file_name + '.csv')
-
 
     #Open and read sql file
     with open(sql_path, "r") as file:
@@ -83,7 +81,7 @@ def sql_query():
 #Pandas processing and Transformation
 def pandas_process():
     '''
-    This function uses pandas to normalize data in .csv and stores it in /dataset folder as a .csv
+    This function uses pandas to normalize data in .csv and stores it in /dataset folder
     '''
     #Load university dataframe
     df_uni = pd.read_csv(os.path.join(air_root_folder, 'files/' + file_name + '.csv'))
@@ -96,49 +94,69 @@ def pandas_process():
     for name_col in special_cols:
         df_uni[name_col] = df_uni[name_col].astype(str).apply(lambda x: x.lower()).apply(lambda x: x.strip()).apply(lambda x:x.replace('-', ' '))
 
-    #Change gender col to category type
+
+    #Gender values Renaming
     df_uni["gender"] = df_uni["gender"].map({'M' : 'male', 'F' : 'female'})
+    # Change gender col to category type
     df_uni["gender"] = df_uni["gender"].astype('category')
 
     #Inscription date
-    df_uni["inscription_date"] = pd.to_datetime(df_uni["inscription_date"],infer_datetime_format=True)
+    df_uni["inscription_date"] = pd.to_datetime(df_uni["inscription_date"], infer_datetime_format=True)
     df_uni['inscription_date'] = df_uni['inscription_date'].astype(str)
 
-    #Age
-    df_uni['birth_date'] = pd.to_datetime(df_uni['birth_date'],infer_datetime_format=True)
-    def calc_age(birth_col):
+    def age(birth_date):
         '''
-        Given a birth date columns it returns the years according to current date
+        given the birth date returns the age
         '''
-        today = pd.to_datetime(date.today())  # convert today to a pandas datetime
-        return (today - birth_col) / pd.Timedelta(days=365.25)  # divide by days to get years
+        today = date.today()
+        if file_name == 'F_uni_nacional_de_rio_cuarto':
+            birth_date = datetime.strptime(birth_date, "%y/%b/%d").date()
+            if birth_date.year > today.year:
+                birth_date_l = list(str(birth_date))
+                birth_date_l[0] = '1'
+                birth_date_l[1] = '9'
+                birth_date_s = "".join(birth_date_l)
+                birth_date = datetime.strptime(birth_date_s, "%Y-%m-%d").date()
+        elif file_name == 'F_uni_de_moron':
+            birth_date = datetime.strptime(birth_date, "%d/%m/%Y").date()
+        
 
-    df_uni['age'] = calc_age(df_uni['birth_date'])
-    df_uni['age']= df_uni['age'].astype(int)
+        return today.year - birth_date.year - ((today.month,today.day) < (birth_date.month, birth_date.day))
+
+    df_uni['age'] = df_uni['birth_date'].apply(age)
 
     #Load postal code dataframe
     df_pc = pd.read_csv(air_root_folder + '/assets/codigos_postales.csv')
-    
     #working with postal_code
     df_pc.columns = ['postal_code', 'location']
     df_pc['location'] = df_pc['location'].apply(lambda x: x.lower()).apply(lambda x: x.strip()).apply(lambda x:x.replace('-', ' '))
     df_pc.drop_duplicates(subset=['location'], keep="first", inplace=True)
-    #merge df_uni with df_pc on 'postal_code'
-    df = df_uni.merge(df_pc, how="left", on='postal_code', copy="false")
-    df.drop('location_x', axis = 1, inplace = True)
-    df.rename(columns = {'location_y':'location'}, inplace = True)
+    
+    #Merging with postal code
+    if file_name == 'F_uni_nacional_de_rio_cuarto':
+        #merge df_uni with df_pc on 'location'
+        df = df_uni.merge(df_pc, how="left", on='location', copy="false")
+        df.drop('postal_code_x', axis = 1, inplace = True)
+        df.rename(columns = {'postal_code_y':'postal_code'}, inplace = True)
+    
+    elif file_name == 'F_uni_de_moron':
+        #merge df_uni with df_pc on 'postal_code'
+        df = df_uni.merge(df_pc, how="left", on='postal_code', copy="false")
+        df.drop('location_x', axis = 1, inplace = True)
+        df.rename(columns = {'location_y':'location'}, inplace = True)
 
+    
     #We keep just with cols of interest
     df = df[['university', 'career', 'inscription_date', 'last_name', 'gender', 'age', 'postal_code', 'location', 'email']]
     
     #Dataset path
     df_path = air_root_folder + '/datasets/' + file_name + '.csv'
+    
     #Dataset folder creation
     os.makedirs(os.path.dirname(df_path), exist_ok=True)
     
     #Log
     logger.info('Saving file')
-
 
     #Save df final version in /dataset as a csv file
     df.to_csv(df_path)
@@ -164,15 +182,15 @@ def load_to_S3():
     logger.info('The file was succesfully load into s3')
     
 
-
 #Instantiate DAG
-with DAG('F_uni_de_moron',
-         description = ' dag for Universidad de Moron',
+with DAG("F_DD_uni_nacional_de_rio_cuarto",
+         description = "DAG for Universidad Nacional de Rio Cuarto",
          start_date=datetime(2022, 8, 23),
-         schedule_interval='@hourly',
+         schedule_interval= "@hourly",
          default_args = default_args,
          catchup=False
         ) as dag:
+
 
         
         #First task: Extraction. (PostgresOperator could be used)
@@ -192,7 +210,5 @@ with DAG('F_uni_de_moron',
             task_id='load_to_S3',
             python_callable = load_to_S3
         )
-
-
 
         opr_sql_query >> opr_pandas_process >> opr_load_to_S3
